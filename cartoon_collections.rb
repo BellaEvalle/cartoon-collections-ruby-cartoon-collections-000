e47a5c555cf04b20887c6fa5c0d0783022249e73
Bella Evalle <bellaevalle@yahoo.com>
2020-04-06 21:29:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def roll_call_dwarves (dwarves)
-dwarves.each_with_index { |name, index| dwarves.length / 2}
+dwarves.each_with_index do |name, index|
+  if dwarves.length / 2 > index
+   puts "#{index + 1}. #{name}"
+ end
+end
 end
 
 def summon_captain_planet (planeteer_calls)
